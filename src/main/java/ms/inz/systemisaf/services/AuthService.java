02207e62589fd0887daf0df91d2ff27f9d5d57cb
mps2k1 <mateuszsalwowski2001@gmail.com>
2025-01-02 21:01:43 +0100

    add validation

--- src/main/java/ms/inz/systemisaf/services/AuthService.java
+++ src/main/java/ms/inz/systemisaf/services/AuthService.java
@@ -1,11 +1,9 @@
 package ms.inz.systemisaf.services;
 
-import ms.inz.systemisaf.config.CustomUserDetails;
 import ms.inz.systemisaf.dto.UserRegistrationDto;
 import ms.inz.systemisaf.enums.RoleEnum;
 import ms.inz.systemisaf.model.User;
 import ms.inz.systemisaf.repositories.UserRepository;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,7 +28,16 @@ public class AuthService {
         if (userRepository.existsByEmail(userDto.getEmail())) {
             throw new IllegalArgumentException("Email already exists");
         }
+        if (userDto.getEmail().isBlank()){
+            throw new IllegalArgumentException("Email cannot be blank");
+        }
 
+        if (!userDto.getEmail().contains("@")) {
+            throw new IllegalArgumentException("Email must contain '@'");
+        }
+        if (userDto.getPassword().length() < 8) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long");
+        }
         if (userDto.getRole() == null) {
             throw new IllegalArgumentException("Role is required");
         }
